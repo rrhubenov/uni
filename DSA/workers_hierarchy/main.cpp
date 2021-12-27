dc827cadd2c57054f5a79bf529c004c593db5955
Radoslav Hubenov <rrhubenov@gmail.com>
2021-12-27 19:52:01 +0200

    remove and ignore ds store

--- DSA/workers_hierarchy/main.cpp
+++ DSA/workers_hierarchy/main.cpp
@@ -4,12 +4,32 @@
 #include <string>
 using namespace std;
 
-int main() {
-    string input = string("Uspeshnia-Gosho\nUspeshnia-Misho\nUspeshnia-Slavi\nGosho-Dancho\nGosho-Pesho\n");
-    Hierarchy h1 = Hierarchy(input);
+const string lozenec =
+    "Uspeshnia - Gosho \n"
+    "Uspeshnia - Misho \n"
+    "Gosho     - Pesho \n"
+    "Gosho     - Dancho\n"
+    "Pesho     - Alex  \n"
+    "Dancho    - Boris \n"
+    "Dancho    - Kamen \n"
+    "Uspeshnia - Slavi \n"
+    "Slavi     - Slav1 \n"
+    "Slavi     - Slav2 \n"
+    "Slav1     - Mecho \n"
+    "Mecho     - Q12Adl\n";
 
-    cout << h1.print();
-    cout << h1.longest_chain();
+const string loz_new =
+    " Uspeshnia   -  MishoPetrov \n"
+    " MishoPetrov -  Misho       \n"
+    " MishoPetrov -  Slav        \n";
+
+int main() {
+    Hierarchy h1 = Hierarchy(lozenec);
+    Hierarchy h2 = Hierarchy(loz_new);
+
+    Hierarchy joined = h2.join(h1);
+
+    cout << joined.print() << endl;
 
     return 0;
 }
