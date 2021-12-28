3ff3a06b2e9671ae35f197bbc8775531c0985387
Radoslav Hubenov <rrhubenov@gmail.com>
2021-12-28 22:22:43 +0200

    commands headers

--- DSA/workers_hierarchy/headers/commands.hh
+++ DSA/workers_hierarchy/headers/commands.hh
@@ -19,10 +19,10 @@ class CLIController {
     private:
         vector<ICommand*> commands; 
 
+    public:
+
         void execute_command(string command_name,
                 vector<string> args); 
-
-    public:
         void begin_reading(); 
         void register_command(ICommand* command); 
         vector<ICommand*> get_commands();
@@ -44,8 +44,11 @@ class Help: public ICommand {
 };
 
 class Exit: public ICommand {
+    private:
+        vector<pair<string, Hierarchy>>& hierarchies;
+        CLIController* cliController;
     public:
-        Exit();
+        Exit(vector<pair<string, Hierarchy>>& hierarchies, CLIController* cliController);
 
         void execute(vector<string> args);
         string getName();
@@ -70,6 +73,138 @@ class Save: public ICommand {
 
     public:
         Save(vector<pair<string, Hierarchy>>& hierarchies);
+
+        void execute(vector<string> args);
+        string getName();
+        string getDetails();
+};
+
+class Find: public ICommand {
+    private:
+        vector<pair<string, Hierarchy>>& hierarchies;
+
+    public:
+        Find(vector<pair<string, Hierarchy>>& hierarchies);
+
+        void execute(vector<string> args);
+        string getName();
+        string getDetails();
+};
+
+class NumSubordinates: public ICommand {
+    private:
+        vector<pair<string, Hierarchy>>& hierarchies;
+
+    public:
+        NumSubordinates(vector<pair<string, Hierarchy>>& hierarchies);
+
+        void execute(vector<string> args);
+        string getName();
+        string getDetails();
+};
+
+class Manager: public ICommand {
+    private:
+        vector<pair<string, Hierarchy>>& hierarchies;
+
+    public:
+        Manager(vector<pair<string, Hierarchy>>& hierarchies);
+
+        void execute(vector<string> args);
+        string getName();
+        string getDetails();
+};
+
+class NumEmployees: public ICommand {
+    private:
+        vector<pair<string, Hierarchy>>& hierarchies;
+
+    public:
+        NumEmployees(vector<pair<string, Hierarchy>>& hierarchies);
+
+        void execute(vector<string> args);
+        string getName();
+        string getDetails();
+};
+
+class Overloaded: public ICommand {
+    private:
+        vector<pair<string, Hierarchy>>& hierarchies;
+
+    public:
+        Overloaded(vector<pair<string, Hierarchy>>& hierarchies);
+
+        void execute(vector<string> args);
+        string getName();
+        string getDetails();
+};
+
+class Join: public ICommand {
+    private:
+        vector<pair<string, Hierarchy>>& hierarchies;
+
+    public:
+        Join(vector<pair<string, Hierarchy>>& hierarchies);
+
+        void execute(vector<string> args);
+        string getName();
+        string getDetails();
+};
+
+class Fire: public ICommand {
+    private:
+        vector<pair<string, Hierarchy>>& hierarchies;
+
+    public:
+        Fire(vector<pair<string, Hierarchy>>& hierarchies);
+
+        void execute(vector<string> args);
+        string getName();
+        string getDetails();
+};
+
+class Hire: public ICommand {
+    private:
+        vector<pair<string, Hierarchy>>& hierarchies;
+
+    public:
+        Hire(vector<pair<string, Hierarchy>>& hierarchies);
+
+        void execute(vector<string> args);
+        string getName();
+        string getDetails();
+};
+
+class Salary: public ICommand {
+    private:
+        vector<pair<string, Hierarchy>>& hierarchies;
+
+    public:
+        Salary(vector<pair<string, Hierarchy>>& hierarchies);
+
+        void execute(vector<string> args);
+        string getName();
+        string getDetails();
+};
+
+class Incorporate: public ICommand {
+    private:
+        vector<pair<string, Hierarchy>>& hierarchies;
+
+    public:
+        Incorporate(vector<pair<string, Hierarchy>>& hierarchies);
+
+        void execute(vector<string> args);
+        string getName();
+        string getDetails();
+};
+
+class Modernize: public ICommand {
+    private:
+        vector<pair<string, Hierarchy>>& hierarchies;
+
+    public:
+        Modernize(vector<pair<string, Hierarchy>>& hierarchies);
 
         void execute(vector<string> args);
         string getName();
