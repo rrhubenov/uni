09722e0e33fd149fa9d86834525a48355b69663b
Radoslav Hubenov <rrhubenov@gmail.com>
2022-01-15 22:52:15 +0200

    use monad for keeping track of next t var

--- FP/type_inference/main.hs
+++ FP/type_inference/main.hs
@@ -1,4 +1,38 @@
-import Data.Kind ()
+-- State management
+type State = Char
+newtype ST a = S (State -> (a, State))
+
+app :: ST a -> State -> (a, State)
+app (S st) = st
+
+instance Functor ST where
+    -- fmap :: (a->b) -> ST a -> ST b
+    fmap f (S st) = S(\state -> let (a, s) = st state in (f a, s))
+
+instance Applicative ST where
+    -- pure :: a -> ST a
+    pure a = S(\state -> (a, state))
+
+    -- <*> :: ST (a -> b) -> ST a -> ST b
+    (<*>) (S fst) (S ast) = S(\s ->
+        let
+            (f, s') = fst s
+            (a, s'') = ast s'
+            in (f a, s'')
+        )
+
+instance Monad ST where
+    -- return :: a -> ST a
+    return a = S(\state -> (a, state))
+
+    -- >>= :: ST a -> (a -> ST b) -> ST b
+    st >>= f = S (\s ->
+        let (a, s') = app st s
+        in app (f a) s')
+
+fresh :: ST Type
+fresh = S (\c -> (TVar c, succ c))
+--
 
 data Term
     = Var String
@@ -7,7 +41,8 @@ data Term
     deriving (Show, Eq)
 
 data Type
-    = TVar String
+    -- = TVar String
+    = TVar Char
     | Fun Type Type
     deriving (Eq)
 
@@ -34,13 +69,10 @@ parse term
           remainingTerm = takeWhile (/= '(') leftTerm
           extractedTerm = tail $ init $ dropWhile (/= '(') term
 
-fnResult (Fun from to) = to
-fnResult _ = error "only for functions"
-
 -- map of variables to types
 type Context = (String, Type)
 -- map of type variables to types
-type Substitution = (String, Type)
+type Substitution = (Char, Type)
 
 find :: (p -> Bool) -> [p] -> p
 find p []     = error "Not found"
@@ -49,7 +81,7 @@ find p (x:xs) = if p x then x else find p xs
 findAssumption :: String -> [Context] -> Type
 findAssumption term ctx = snd $ find (\ (var, ty) -> var == term) ctx
 
-findSubstitution :: String -> [Substitution] -> Type
+findSubstitution :: Char -> [Substitution] -> Type
 findSubstitution term subs = snd $ find (\ (cterm, ctype) -> cterm == term) subs
 
 applySubstitution :: [Substitution] -> Type -> Type
@@ -74,26 +106,17 @@ substUnion s1 [] = s1
 substUnion [] s2 = s2
 substUnion s1 (s:ss) = if s `elemSubs` s1 then substUnion s1 ss else substUnion (s:s1) ss
 
-s1 :: Substitution
-s1 = ("a", TVar "a")
-s2 :: Substitution
-s2 = ("a", TVar "b")
-s3 :: Substitution
-s3 = ("a", Fun (TVar "b") (TVar "a"))
-
 composeSubstitutions :: [Substitution] -> [Substitution] -> [Substitution]
 composeSubstitutions s1 s2 = substUnion (reverse (map (\ (tvar, ty) -> (tvar, applySubstitution s1 ty)) s2)) s1
-
--- [("x",TFun (TVar "a") (TVar "b")),("y",TVar "b")]
 
 uniq [] = []
 uniq (x:xs) = if x `elem` xs then uniq xs else x:uniq xs
 
-freeTypeVars :: Type -> [String]
+freeTypeVars :: Type -> [Char]
 freeTypeVars (TVar var) = [var]
-freeTypeVars (Fun t1 t2) = uniq $ (freeTypeVars t1) ++ (freeTypeVars t2)
+freeTypeVars (Fun t1 t2) = uniq $ freeTypeVars t1 ++ freeTypeVars t2
 
-varBind :: String -> Type -> [Substitution]
+varBind :: Char -> Type -> [Substitution]
 varBind var ty
     | ty == TVar var = []
     | var `elem` freeTypeVars ty = error "occurs check"
@@ -107,23 +130,22 @@ unify (Fun arg1 res1) (Fun arg2 res2) = composeSubstitutions s1 s2
 unify (TVar var) ty = varBind var ty
 unify ty (TVar var) = varBind var ty
 
-infer :: Term -> [Context] -> Char -> ([Substitution], Type)
-infer (Var var) ctx sv = ([], findAssumption var ctx)
-infer (Lam var body) ctx sv = (s1, Fun (applySubstitution s1 tyVar) tyBody)
-    where
-        tyVar = TVar [sv]
-        newctx = (var, tyVar):ctx
-        (s1, tyBody) = infer body newctx (succ sv)
-infer (App fun arg) ctx sv = (subst, applySubstitution s3 tyRes)
-    where
-        tyRes = TVar [sv]
-        (s1, tyFun) = infer fun ctx (succ sv)
-        (s2, tyArg) = infer arg (applySubstitutionCtx s1 ctx) (succ $ succ sv)
-        s3 = unify (applySubstitution s2 tyFun) (Fun tyArg tyRes)
-        subst = composeSubstitutions s3 (composeSubstitutions s2 s1)
+infer :: Term -> [Context] -> ST ([Substitution], Type)
+infer (Var var) ctx = return ([], findAssumption var ctx)
+infer (Lam var body) ctx =
+    do
+        tyVar <- fresh
+        let newctx = (var, tyVar):ctx
+        (s1, tyBody) <- infer body newctx
+        return (s1, Fun (applySubstitution s1 tyVar) tyBody)
+infer (App fun arg) ctx =
+    do
+        tyRes <- fresh
+        (s1, tyFun) <- infer fun ctx
+        (s2, tyArg) <- infer arg (applySubstitutionCtx s1 ctx)
+        let s3 = unify (applySubstitution s2 tyFun) (Fun tyArg tyRes)
+        let subst = composeSubstitutions s3 (composeSubstitutions s2 s1)
+        return (subst, applySubstitution s3 tyRes)
 
--- ПРОБЛЕМИ:
--- Не знам как да генерирам нови уникални tvar имена. Засега ползвам succ с някакъв начален char, но това очевидно няма да работи коректно
--- Трябва да разбера по-подробно какво се случва
-
-
+run :: String -> Type
+run input = snd $ fst $ app (infer (parse input) []) 'a'
