3e2f23f0a0d639bf37bd78aec014d54a7479eced
Radoslav Hubenov <rrhubenov@gmail.com>
2022-01-25 22:12:43 +0200

    call parser in main

--- DSA/listFunc/main.cpp
+++ DSA/listFunc/main.cpp
@@ -1,17 +1,19 @@
 #include "headers/lexer.hh"
+#include "headers/parser.hh"
 #include <iostream>
 
 using namespace std;
 
 int main() {
-
+    string line;
     Lexer l = Lexer();
+    Parser p = Parser();
 
-    vector<Token> ts = l.tokenize("[ 42.69 ] (())");
-
-    for(auto t : ts) {
-        cout << t.name << " " << t.value << endl;
+    while(getline(cin, line)) {
+        vector<Token> ts = l.tokenize("[ 42.69 ] (())");
+        Node* ast = p.parse(ts);
     }
+
 
     return 0;
 }
