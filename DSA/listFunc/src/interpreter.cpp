0fc391764e6b80d034403e5d4757d21f4b26a1a5
Radoslav Hubenov <rrhubenov@gmail.com>
2022-07-12 12:16:07 +0300

    removing old list func project

--- DSA/listFunc/src/interpreter.cpp
+++
@@ -1,9 +0,0 @@
-#include "../headers/interpreter.hh"
-#include "../headers/parser.hh"
-#include <unordered_map>
-
-
-string Interpreter::eval(Node* ast, unordered_map<string, Node*>& c) {
-    Value* result = ast->eval(c);
-    return result->print();
-}
